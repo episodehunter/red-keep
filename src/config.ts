754d3dca9b729527a873fc6c4c7ae46764f93179
tjoskar <kontakta@oskarkarlsson.nu>
2018-02-03 15:37:04 +0100

    🎨  Default to local database

--- src/config.ts
+++ src/config.ts
@@ -4,11 +4,11 @@ export const config = {
   db: {
     client: 'mysql',
     connection: {
-      host: process.env.EH_DB_HOST,
-      user: process.env.EH_DB_USERNAME,
-      password: process.env.EH_DB_PASSWORD,
-      database: process.env.EH_DB_DATABASE,
-      port: process.env.EH_DB_PORT
+      host: process.env.EH_DB_HOST || '127.0.0.1',
+      user: process.env.EH_DB_USERNAME || 'user',
+      password: process.env.EH_DB_PASSWORD || '123',
+      database: process.env.EH_DB_DATABASE || 'episodehunter',
+      port: process.env.EH_DB_PORT || 3306
     },
     debug: false
   } as Config
