b452151fd883c6cc76b1d606f52cc7922771a93a
tjoskar <kontakta@oskarkarlsson.nu>
2018-02-24 11:16:42 +0100

    🚑  Extract Engine constructor

--- src/engine.ts
+++ src/engine.ts
@@ -1,7 +1,7 @@
 import { config } from './config'
 
 if (!config.inDevelopMode) {
-  const Engine = require('apollo-engine')
+  const { Engine } = require('apollo-engine')
   const engine = new Engine({
     engineConfig: {
       apiKey: process.env.APOLLO_ENGINE_API_KEY
